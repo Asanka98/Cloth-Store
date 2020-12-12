bf9cbdb775d251ae8d54188568cc83526dfb324a
Piumi <aluviharepiumee@gmail.com>
2020-12-12 21:46:11 +0530

    package creation 2020 12 12

--- Clothes-Store/src/main/java/com/shop/controller/HomeController.java
+++ Clothes-Store/src/main/java/com/shop/controller/HomeController.java
@@ -1,16 +1,17 @@
-package com.shop.app;
+package com.shop.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
-	
-	@RequestMapping("/")
-	public String index() {
-		
-		
-		return "first";
-	}
+
+ @RequestMapping("/home")
+  public String home() {
+
+	 
+  return "home";
+  
+  }
 
 }
